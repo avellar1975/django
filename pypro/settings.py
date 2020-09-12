4a1a58818e982eb1f157982afb92192eae304694
avellar1975 <evandro.avellar@gmail.com>
2020-09-12 13:30:27 -0300

    Correção settings

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
 from functools import partial
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
