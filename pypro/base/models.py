ccecdb4480369f4a0dbe5aad32a1d68b5286d397
avellar1975 <evandro.avellar@gmail.com>
2021-01-13 17:12:14 -0300

    Pep8

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -9,11 +9,15 @@ from django.core.mail import send_mail
 
 
 class UserManager(BaseUserManager):
+    """Docstring de Classe."""
+
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given username, email, and password.
+        """Docstring de função.
+
+        Create and save a user with the given
+        username, email, and password.
         """
         if not email:
             raise ValueError('The given username must be set')
@@ -24,10 +28,12 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
+        """Docstring de funçao."""
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
+        """Docstring de funçao."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -51,7 +57,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
-        help_text=_('Designates whether the user can log into this admin site.'),
+        help_text=_('Designates whether the user can log into this'
+                    'admin site.'),
     )
     is_active = models.BooleanField(
         _('active'),
@@ -70,15 +77,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     class Meta:
+        """Docstring de Classe."""
+
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
     def clean(self):
+        """Docstring de funçao."""
         super().clean()
         self.email = self.__class__.objects.normalize_email(self.email)
 
     def get_full_name(self):
-        """
+        """Docstring de funçao.
+
         Return the first_name plus the last_name, with a space in between.
         """
         full_name = '%s' % (self.first_name)
